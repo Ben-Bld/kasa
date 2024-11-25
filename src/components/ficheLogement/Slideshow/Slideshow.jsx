4ea3603aa6cca8e761543e3285c1812f82776077
Ben-Bld <blanchard.benoit3@gmail.com>
2024-11-25 13:11:16 +0100

    animation slider

--- src/components/ficheLogement/Slideshow/Slideshow.jsx
+++ src/components/ficheLogement/Slideshow/Slideshow.jsx
@@ -1,59 +1,61 @@
-
-import left from '../../../assets/images/icons/left.svg'
-import right from '../../../assets/images/icons/right.svg'
+import left from '../../../assets/images/icons/left.svg';
+import right from '../../../assets/images/icons/right.svg';
 import { useState } from "react";
 
-
 export function Slideshow(props) {
+    let slider = props.slide;
 
-    let slider = props.slide
+    const [slideValue, changeSlide] = useState(0);
+    const [fade, setFade] = useState(false); // New state for fade animation
+    let length = props.slide.length - 1;
 
-
-    const [slideValue, changeSlide] = useState(0)
-    let length = props.slide.length - 1
     function nextSlide() {
-        if (slideValue < length) {
-            changeSlide(slideValue + 1)
-        }
-        else {
-
-            changeSlide(slideValue - (slideValue))
-        }
-
-
+        setFade(true); // Trigger fade-out animation
+        setTimeout(() => {
+            setFade(false); // Reset fade state
+            if (slideValue < length) {
+                changeSlide(slideValue + 1);
+            } else {
+                changeSlide(0);
+            }
+        }, 500); // Match the animation duration
     }
+
     function prevSlide() {
-        if (slideValue > 0) {
-            changeSlide(slideValue - 1);
-        } else {
-            changeSlide(length);
-        }
+        setFade(true); // Trigger fade-out animation
+        setTimeout(() => {
+            setFade(false); // Reset fade state
+            if (slideValue > 0) {
+                changeSlide(slideValue - 1);
+            } else {
+                changeSlide(length);
+            }
+        }, 500); // Match the animation duration
     }
 
     function hideNext() {
-        if (length == 0) {
-            return "hide"
+        if (length === 0) {
+            return "hide";
         }
     }
-
 
     return (
         <div className="container-slideshow">
             <div className="container-slideshow__leftArrow">
-                <img onClick={prevSlide} src={left} className={`container__slideshow__lefttArrow__file ` + hideNext()}></img>
-
-
-
+                <img onClick={prevSlide} src={left} className={`container__slideshow__leftArrow__file ` + hideNext()} />
             </div>
             <div className="container-slideshow__rightArrow">
-                <img onClick={nextSlide} src={right} className={`container__slideshow__rightArrow__file ` + hideNext()}></img>
+                <img onClick={nextSlide} src={right} className={`container__slideshow__rightArrow__file ` + hideNext()} />
             </div>
-
             <div className="container-slideshow__pageNumber ">
                 <p className={hideNext()}>{slideValue + 1}/{slider.length}</p>
             </div>
 
-            <img className="container-slideshow__img" src={slider[slideValue]} alt="" />
+            <img
+                className={`container-slideshow__img ${fade ? "fade-out" : "fade-in"}`}
+                src={slider[slideValue]}
+                alt=""
+            />
         </div>
-    )
+    );
 }
